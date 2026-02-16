c33113009df720c7eb5a6c694ba8bae58a1780c9
mahfuzurrahman01 <mafuzur.rahman032@gmail.com>
2026-02-16 20:38:06 +0600

    default dashboard screen updated

--- apps/isomorphic-intl/src/app/[locale]/(hydrogen)/page.tsx
+++ apps/isomorphic-intl/src/app/[locale]/(hydrogen)/page.tsx
@@ -1,10 +1,11 @@
 import { metaObject } from "@/config/site.config"
 import FinancialDashboard from "@/modules/fms/components/templates/financial-dashboard"
+import HrmsDashboardPage from "./hrms/dashboard/page"
 
 export const metadata = {
   ...metaObject("Dashboard"),
 }
 
 export default function FileDashboardPage() {
-  return <FinancialDashboard />
+  return <HrmsDashboardPage />
 }
